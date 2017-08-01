59acfbac165a63f06e28da5c0730233cc1e2e439
Gary Lloyd <gareth.lloyd@tessella.com>
2017-08-01 16:35:00 +0100

    Fix: Update symbolic link to use SECRET_CONFIG path

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,7 @@ echo "SECRET_CONFIG - ${SECRET_CONFIG}"
 if [[ -v SECRET_CONFIG ]]; then
   echo "Configuring Shiro config to use provided secret at ${SECRET_CONFIG}"
   
-  ln -s /secret/shiro.ini /opt/zeppelin/conf/shiro.ini
+  ln -s ${SECRET_CONFIG} /opt/zeppelin/conf/shiro.ini
 else
   echo "Using default Shiro configuration"
 fi
